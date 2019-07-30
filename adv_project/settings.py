6a052b775112ab6d5d703a17b199e8cd9bec07e1
josephriosio <blankworldwide@gmail.com>
2019-07-30 09:47:20 -0700

    added database

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -23,11 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-DATABASES['DATABASE_URL'] = dj_database_url.config(conn_max_age=600)
-DATABASES['DATABASE_URL'] = dj_database_url.config(
-    default='postgres://USER:PASSWORD@HOST:PORT/NAME')
-DATABASES['DATABASE_URL'] = dj_database_url.parse(
-    'postgres://USER:PASSWORD@HOST:PORT/NAME', conn_max_age=600)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
