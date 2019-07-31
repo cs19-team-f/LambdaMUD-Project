95a04d02acdfb5cc12aa75d49c32de41583a5280
Cameron MacDonald <31353409+cammac725@users.noreply.github.com>
2019-07-30 21:31:46 -0700

    Revert "Cam MacDonald"

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -151,7 +151,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
 # whitenoise static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
