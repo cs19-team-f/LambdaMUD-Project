ba1a9cd10770777d4d7cf32262ea24b074c8be21
cammac <camm@rocktechcomputer.com>
2019-08-01 09:02:45 -0700

    formatting

--- util/create_world.py
+++ util/create_world.py
@@ -18,44 +18,78 @@ r_outside = Room(
 r_foyer = Room(
     title="Foyer", description="""Dim light filters in from the south. Dusty passages run north and east and west.""",
     x_coord=10,
-    y_coord=11)
+    y_coord=11
+)
 
-r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", x_coord=8,
-                  y_coord=13)
+r_overlook = Room(
+    title="Grand Overlook",
+    description="A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.",
+    x_coord=8,
+    y_coord=13
+)
 
-r_greatroom = Room(title="Great Room", description="""A cavernous room, the ceiling disappears in darkness. The cold floor tiles are covered with huge, darkly colored area rugs. Portraits of glaring ancestors hang on the walls, seemingly watching your every move.""", x_coord=12,
-                   y_coord=12)
+r_greatroom = Room(
+    title="Great Room",
+    description="A cavernous room, the ceiling disappears in darkness. The cold floor tiles are covered with huge, darkly colored area rugs. Portraits of glaring ancestors hang on the walls, seemingly watching your every move.",
+    x_coord=12,
+    y_coord=12
+)
 
-r_library = Room(title="Library", description="""Full of dark wood and looming stuffed animal heads, it smells of ominous deeds and stale smoke. Light filters in from the french doors to the patio beyond.""", x_coord=9,
-                 y_coord=12)
+r_library = Room(
+    title="Library",
+    description="Full of dark wood and looming stuffed animal heads, it smells of ominous deeds and stale smoke. Light filters in from the french doors to the patio beyond.",
+    x_coord=9,
+    y_coord=12
+)
 
 r_hallway = Room(
-    title="Hallway", description="""A wide hallway lined with banners and heraldry. Is that garlic?""", x_coord=9,
-    y_coord=11)
+    title="Hallway",
+    description="A wide hallway lined with banners and heraldry. Is that garlic?",
+    x_coord=9,
+    y_coord=11
+)
 
-r_terrace = Room(title="Terrace", description="""A wide stone terrace littered with overturned chairs, empty planter boxes, and dead birds. A few stone steps lead down to the Overlook.""", x_coord=8,
-                 y_coord=12)
+r_terrace = Room(
+    title="Terrace",
+    description="A wide stone terrace littered with overturned chairs, empty planter boxes, and dead birds. A few stone steps lead down to the Overlook.",
+    x_coord=8,
+    y_coord=12
+)
 
-r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", x_coord=11,
-                y_coord=11)
+r_narrow = Room(
+    title="Narrow Passage",
+    description="The narrow passage bends here from west to north. The smell of gold permeates the air.",
+    x_coord=11,
+    y_coord=11
+)
 
-r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", x_coord=11,
-                  y_coord=12)
+r_treasure = Room(
+    title="Treasure Chamber",
+    description="You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.",
+    x_coord=11,
+    y_coord=12
+)
 
-r_basement = Room(title="Dark Basement", description="""You're going to need a torch in here. Crates and boxes stacked everywhere. In the dust on the floor, some faint footprints lead off around the corner of some shelves.""", x_coord=9,
-                  y_coord=10)
+r_basement = Room(
+    title="Dark Basement",
+    description="You're going to need a torch in here. Crates and boxes stacked everywhere. In the dust on the floor, some faint footprints lead off around the corner of some shelves.",
+    x_coord=9,
+    y_coord=10
+)
 
-r_secret = Room(title="Secret Passage",
-                description="""Very narrow but high-ceilinged passage that twists and turns. An acrid smell gets stronger as you walk.""", x_coord=8,
-                y_coord=10)
+r_secret = Room(
+    title="Secret Passage",
+    description="Very narrow but high-ceilinged passage that twists and turns. An acrid smell gets stronger as you walk.",
+    x_coord=8,
+    y_coord=10
+)
 
-r_laboratory = Room(title="Laboratory", description="""Foul odors rise from buckets on the floor. Some of this large equipment must have been assembled pice by piece in here. What is that on the walls? """, x_coord=8,
-                    y_coord=9)
+r_laboratory = Room(
+    title="Laboratory",
+    description="Foul odors rise from buckets on the floor. Some of this large equipment must have been assembled pice by piece in here. What is that on the walls? ",
+    x_coord=8,
+    y_coord=9
+)
 
 rooms = [
     r_outside,
